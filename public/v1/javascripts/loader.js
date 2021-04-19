e1310e728e1f81ddb3a1d47cdd403fb3ddc8e8e6
van luc <htms.group.vn@gmail.com>
2021-04-19 16:01:10 +0700

    them chọn phường xã cho cửa hàng

--- public/v1/javascripts/loader.js
+++ public/v1/javascripts/loader.js
@@ -43,9 +43,11 @@ $(document).ready(function($){
 
 	
 	ojs_loader = {	
-		//
-		//
-		//
+
+		//@
+		//@
+		//@
+		tinh_thanh_datas:[],
 		//Khai báo hosting global
 		//khi đổi tên miền có thể thay đổi biến này
 		//biến này là global
@@ -681,14 +683,94 @@ $(document).ready(function($){
 			ntext = ntext + n_split[i];
 		}
 		return parseInt(ntext);
-	}	
+	},	
+	//@
+	//@
+	//@
+	//lấy danh sách tỉnh thành
+	get_tinh_thanh:function(datas){
+		let data_return = "";
+		let i;
+		let txt_for = '<option value="0"></option>';
+		for(i = 0; i < datas.length ; i ++ ){
+			txt_for = txt_for + '<option value="' +  datas[i].Name + '">' + datas[i].Name + '</option>';
+		}
+		data_return = data_return + txt_for;
+		return data_return;
+	},	
+	//@
+	//@
+	//@
+	//lấy danh sách quận huyện
+	get_quan_huyen:function(datas,taget){
+		let data_return = "";
 		
+		let i;
+		let txt_for = '<option value="0"></option>';
 		
+		//@
+		//@
+		for(i = 0; i < datas.length ; i ++ ){
+			
+			if(datas[i].Name == taget){
+				for(let t = 0; t < datas[i].Districts.length ; t ++ ){
+					txt_for = txt_for + '<option value="' +  datas[i].Districts[t].Name + '">' + datas[i].Districts[t].Name + '</option>';
+				}
+			}
+			
+		}
 		
-		
+		//@
+		//@
+		data_return = data_return + txt_for;
+		return data_return;
+	},	
 
+	//@
+	//@
+	//@
+	//lấy danh sách quận huyện
+	get_phuong_xa:function(datas,taget){
+		let data_return = "";
+		
+		//return data_return;
 		
 		
+		let i;
+		let txt_for = '<option value="0"></option>';
+		
+		//@
+		//@
+		for(i = 0; i < datas.length ; i ++ ){
+			for(let ii = 0; ii < datas[i].Districts.length ; ii ++ ){
+				if(datas[i].Districts[ii].Name == taget){
+					for(let t = 0; t < datas[i].Districts[ii].Wards.length ; t ++ ){
+						txt_for = txt_for + '<option value="' +  datas[i].Districts[ii].Wards[t].Name + '">' + datas[i].Districts[ii].Wards[t].Name + '</option>';
+					}
+				}
+			}
+		}
+		
+		//@
+		//@
+		data_return = data_return + txt_for;
+		return data_return;
+	},			
+	//@
+	//@
+	//@
+	//set value districts
+	set_value_local:function(){
+		let province  = $('#select_stores_province   option:selected').val();
+		let districts = $('#select_stores_district  option:selected').val();
+		let wards = $('#select_stores_wards  option:selected').val();
+		
+		
+		$('#stores_province').attr("data_value",province);
+		$('#stores_district').attr("data_value",districts);
+		$('#stores_wards').attr("data_value",wards);
+		
+	}
 		
 	///////////////////////////
 	//////////////////////////////
