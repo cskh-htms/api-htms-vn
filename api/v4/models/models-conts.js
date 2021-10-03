5225d66e87bad5a51aa6dffa4d4b459e3b2520b0
htmsgroup <htms.group.vn@gmail.com>
2021-10-03 22:05:53 +0700

    20211003 10:00 bản server ok

--- api/v4/models/models-conts.js
+++ api/v4/models/models-conts.js
@@ -8,7 +8,7 @@ const DATABASE_NAME = "dalacenter4";
 const HOST_NAME2 = "103.110.86.240"; //server remote
 const HOST_IP2 = "localhost"; // server user remote
 const DATABASE_USER2 = "remote1";
-const DATABASE_PASS2 = "Vanluc@2003";
+const DATABASE_PASS2 = "Vanluc@2003!@#$%^";
 const DATABASE_NAME2 = "dalacenter4";
 
 
