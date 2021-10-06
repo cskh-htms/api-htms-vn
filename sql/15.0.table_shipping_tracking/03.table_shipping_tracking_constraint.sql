2f91c1ce676675df358def92ade7dc8469d90224
htmsgroup <htms.group.vn@gmail.com>
2021-10-06 20:32:29 +0700

    20211006  20:32 server  shipper

--- sql/15.0.table_shipping_tracking/03.table_shipping_tracking_constraint.sql
+++ sql/15.0.table_shipping_tracking/03.table_shipping_tracking_constraint.sql
@@ -16,7 +16,7 @@ ALTER TABLE `dala_shipping_tracking`
 ADD CONSTRAINT `shipping_tracking_users_id` 
 FOREIGN KEY (`dala_shipping_tracking_users_id`) 
 REFERENCES `dala_users`(`dala_users_ID`) 
-ON DELETE RESTRICT ON UPDATE RESTRICT;
+ON DELETE CASCADE ON UPDATE CASCADE; 
  
  
 --
@@ -25,7 +25,7 @@ ALTER TABLE `dala_shipping_tracking`
 ADD CONSTRAINT `shipping_tracking_orders_id` 
 FOREIGN KEY (`dala_shipping_tracking_orders_id`) 
 REFERENCES `dala_orders_speciality`(`dala_orders_speciality_ID`) 
-ON DELETE RESTRICT ON UPDATE RESTRICT;
+ON DELETE CASCADE ON UPDATE CASCADE; 
 
 
 
