1d6a4a4894976314e7bffb827ad4aa96ebbff35f
van luc <htms.group.vn@gmail.com>
2022-01-05 15:57:11 +0700

    local gitignore go new

---
+++ api/v4/models/models-config.js
@@ -0,0 +1,42 @@
+
+
+
+//require model const
+const models_const = require('./models-conts');
+
+const connection_data = {
+	/*
+    host     			: models_const.HOST_NAME,
+	localAddress 		: models_const.HOST_IP,
+    user     			: models_const.DATABASE_USER,
+    password 			: models_const.DATABASE_PASS,
+    database 			: models_const.DATABASE_NAME,
+	multipleStatements	: true
+	*/
+	
+	host     		: models_const.HOST_NAME2,
+	localAddress 	: models_const.HOST_IP2,
+    user     		: models_const.DATABASE_USER2,
+    password 		: models_const.DATABASE_PASS2,
+    database 		: models_const.DATABASE_NAME2,
+	multipleStatements	: true	
+	
+
+	
+}
+
+//export
+module.exports = { 
+		connection_data
+}
+
+
+
+
+
+
+
+
+
+
+
