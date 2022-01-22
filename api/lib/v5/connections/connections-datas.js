c80ba7eb592312aea520a1127a10783d7fa9e06c
htmsgroup <htms.group.vn@gmail.com>
2022-01-22 10:06:31 +0700

    20220111 server to local cahge link api

--- api/lib/v5/connections/connections-datas.js
+++ api/lib/v5/connections/connections-datas.js
@@ -5,24 +5,21 @@
 const config_database = require('../../../configs/config-database');
 
 const connection_data = {
-	
+	/*
     host     			: config_database.HOST_NAME,
 	localAddress 		: config_database.HOST_IP,
     user     			: config_database.DATABASE_USER,
     password 			: config_database.DATABASE_PASS,
     database 			: config_database.DATABASE_NAME,
 	multipleStatements	: true
+	*/
 	
-	/*
 	host     		: config_database.HOST_NAME2,
 	localAddress 	: config_database.HOST_IP2,
     user     		: config_database.DATABASE_USER2,
     password 		: config_database.DATABASE_PASS2,
     database 		: config_database.DATABASE_NAME2,
-	multipleStatements	: true	
-	*/
-
-	
+	multipleStatements	: true		
 }
 
 //export
