0b8ff13ecf92bf1e0d397e858d5aa64a254c624f
van luc <htms.group.vn@gmail.com>
2022-09-13 16:59:26 +0700

    lam them api fix distinct

---
+++ api/web/v5/controllers/shippings/controllers-shipping-get-all-province-web.js
@@ -0,0 +1,148 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const md5 = require('md5');
+const fs = require('fs');
+
+const ojs_configs = require('../../../../../configs/config');
+const config_database = require('../../../../configs/config-database');
+const config_api = require('../../../../configs/config-api');
+
+
+const ojs_shares_show_errors = require('../../../../shares/' + config_api.API_SHARES_VERSION + '/ojs-shares-show-errors');
+const ojs_shares_others = require('../../../../shares/' + config_api.API_SHARES_VERSION + '/ojs-shares-others.js');
+const ojs_shares_date = require('../../../../shares/' + config_api.API_SHARES_VERSION + '/ojs-shares-date.js');
+const ojs_shares_fetch_data = require('../../../../shares/' + config_api.API_SHARES_VERSION + '/ojs-shares-fetch-data.js');
+
+const fields_insert = require('../../../../lib/' + config_api.API_LIB_VERSION + '/meta-adress/meta-adress-fields-insert.js');
+const fields_get = require('../../../../lib/' + config_api.API_LIB_VERSION + '/meta-adress/meta-adress-fields-get.js');
+
+const check_role = require('../../../../shares/' + config_api.API_SHARES_VERSION + '/check-role');
+//const product_search = require('../../../../lib/' + config_api.API_LIB_VERSION + '/products/product-search.js');
+//const shipping_search = require('../../../../lib/' + config_api.API_LIB_VERSION + '/shippings/shipping-search.js');
+
+
+
+
+//@
+async  function function_export(req, res, next) {
+	
+	//@
+	//@
+	
+	//@
+	// lấy data request
+	try {
+		var token = req.headers['token'];
+		//res.send([token]);
+		//return;
+	}
+	catch(error){
+		var evn = ojs_configs.evn;
+		//evn = "dev";
+		var error_send = ojs_shares_show_errors.show_error( 
+				evn, 
+				error, 
+				"Lỗi get data request , Vui lòng liên hệ admin" 
+			);
+		res.send({ 
+			"error" : "1", 
+			"position" : "api/web/v5/controller/controllers-shipping-get-all-province-web",
+			"message": error_send 
+		}); 
+		return;	
+	}	
+	
+
+
+	//@
+	//@
+	//neu không có token thì trỏ ra login page
+	if(token == "" || token == null || token == undefined){
+		var evn = ojs_configs.evn;
+		//evn = "dev";
+		var error_send = ojs_shares_show_errors.show_error( evn,"Bạn không có quyền truy cập", "Bạn không có quyền truy cập" );
+		res.send({ "error" : "2", "position":"api/web/v5/controller/controllers-shipping-get-all-province-web", "message": error_send } ); 
+		return;
+	}	
+		
+	//res.send(de_token);
+	//return;
+
+
+
+
+	//@ check role phân quyền
+	const check_role_result = await check_role.check_role(token,res);
+	if(
+	check_role_result == "customer"
+	){
+		//go
+	}
+	else{
+		var evn = ojs_configs.evn;
+		//evn = "dev";
+		var error_send = ojs_shares_show_errors.show_error( 
+				evn, 
+				check_role_result, 
+				"Lỗi phân quyền, Vui lòng liên hệ admin" 
+			);
+		res.send({ 
+			"error" : "22",
+			"position" : "api/web/v5/controller/controllers-shipping-get-all-province-web",
+			"message": error_send 
+		}); 
+		return;			
+	}
+
+
+
+
+	try {
+	    const data = fs.readFileSync(__dirname + '\\local.json', 'utf8');
+		res.send([data]);
+		return;
+	} catch (err) {
+	   res.send([err]);
+	   return;
+	}	
+	//res.send(["dsadasd"]);
+	//return;
+
+
+}
+
+module.exports = function_export;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- api/web/v5/routers/shippings/routers-shipping-web.js
+++ api/web/v5/routers/shippings/routers-shipping-web.js
@@ -9,14 +9,14 @@ const middle_ware =  require('../../../../lib/' + config_api.API_LIB_VERSION + '
 
 
 const controllers_shipping_caution_web =  require('../../controllers/shippings/controllers-shipping-caution-web.js');
-
+const controllers_shipping_get_all_province_web =  require('../../controllers/shippings/controllers-shipping-get-all-province-web.js');
 
 router.get('/', function(req, res, next) {
   res.end('App shipping v5 welcom ');
 });
 
 router.post('/caution/', middle_ware, controllers_shipping_caution_web);
-
+router.get('/get-all-province/', middle_ware, controllers_shipping_get_all_province_web);
 
 
 module.exports = router;
