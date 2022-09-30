f146994b7610ca1e27dba2274838571f2ca9dd0f
van luc <htms.group.vn@gmail.com>
2022-09-30 14:20:36 +0700

    lam api lay danh sach category theo cua hang

--- api/web/v5/routers/categorys/routers-category-web.js
+++ api/web/v5/routers/categorys/routers-category-web.js
@@ -9,12 +9,16 @@ const middle_ware =  require('../../../../lib/' + config_api.API_LIB_VERSION + '
 const controllers_category_web =  require('../../controllers/categorys/controllers-category-web.js');
 const controllers_category_by_parent_web =  require('../../controllers/categorys/controllers-category-by-parent-web.js');
 const controllers_category_by_id_web =  require('../../controllers/categorys/controllers-category-by-id-web.js');
-
+const controllers_category_by_store_id_web =  require('../../controllers/categorys/controllers-category-by-store-id-web.js');
 
 
 router.get('/', middle_ware,controllers_category_web);
 router.get('/by-parent', middle_ware,controllers_category_by_parent_web);
 
 router.get('/by-id', middle_ware,controllers_category_by_id_web);
+router.get('/by-store-id', middle_ware,controllers_category_by_store_id_web);
+
+
+
 
 module.exports = router;
