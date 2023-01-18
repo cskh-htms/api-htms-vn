4dd680711686b9f051c6449b44d1ab8b86d3ad97
van luc <htms.group.vn@gmail.com>
2023-01-18 14:55:25 +0700

    20230118 14:32 - sua link xoong

---
+++ api/lib/v5/connections/connections-datas.js
@@ -0,0 +1,44 @@
+
+
+
+//require model const
+const config_database = require('../../../configs/config-database');
+const config = require('../../../../configs/config');
+
+
+
+if(config.domain == "http://localhost:2021"){
+	var connection_data = {		
+		host     			: config_database.HOST_NAME,
+		localAddress 		: config_database.HOST_IP,
+		user     			: config_database.DATABASE_USER,
+		password 			: config_database.DATABASE_PASS,
+		database 			: config_database.DATABASE_NAME,
+		multipleStatements	: true
+	}
+}else{
+	var connection_data = {
+		host     		: config_database.HOST_NAME2,
+		localAddress 	: config_database.HOST_IP2,
+		user     		: config_database.DATABASE_USER2,
+		password 		: config_database.DATABASE_PASS2,
+		database 		: config_database.DATABASE_NAME2,
+		multipleStatements	: true
+	}	
+}
+
+//export
+module.exports = { 
+		connection_data
+}
+
+
+
+
+
+
+
+
+
+
+
